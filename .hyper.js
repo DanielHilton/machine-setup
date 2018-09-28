3eb742bbdc9c41f8b13da68c1fb0eb44aefc4b21
Daniel Hilton <daniel.hilton@bglgroup.co.uk>
2018-09-28 17:50:14 +0100

    Hyper-monokai

--- .hyper.js
+++ .hyper.js
@@ -131,7 +131,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyper-material-theme", "hyper-search", "hyper-pane"],
+  plugins: ["hyper-search", "hyper-pane", "hyper-monokai"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
